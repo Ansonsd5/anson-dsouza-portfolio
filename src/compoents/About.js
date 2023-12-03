a0ddaac1b1dfb44adfcbce6d57d80d8a9e35b1f8
Ansonsd5 <anson11sd@gmail.com>
2023-12-03 23:31:05 +0530

    fix: ui changes for consistent gaps

--- src/compoents/About.js
+++ src/compoents/About.js
@@ -4,8 +4,9 @@ import { ABOUTTEXT, SELF_IDENTITY } from '../utils/textConstant';
 const About = () => {
   return (
     <div className='flex items-center justify-center mx-auto flex-col font-mono font-[600]'>
-        
-        <article className='leading-loose mt-6 mb-8'>
+     
+        <article className='leading-loose mt-6'>
+          
         <div className='text '>{ABOUTTEXT.welcomeText}</div>
         <div>I am<span className='uppercase text-xl my-underline font-semibold ml-3 text-[#169c65] font-[ui-sans-serif]'>{ABOUTTEXT.myName}</span></div>
         <div className=''>{ABOUTTEXT.empoweringText}</div>
@@ -13,7 +14,8 @@ const About = () => {
         </article>
      
         <article className='p-4  about-me relative'>
-        <div className='mb-2'>{SELF_IDENTITY.ABOUTME}</div>
+        <h2 className='text-[24px] leading-10 text-[#169c65]'>About Me</h2>
+        <div >{SELF_IDENTITY.ABOUTME}</div>
         </article>
         
 
