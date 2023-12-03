9af4ac017a93b9f99ea201112c04d1fd2f98981b
Ansonsd5 <anson11sd@gmail.com>
2023-12-03 18:50:55 +0530

    fix :  footer with props

--- src/compoents/Footer.js
+++ src/compoents/Footer.js
@@ -1,21 +1,19 @@
-import React from 'react'
-import { socialConnections } from '../utils/config'
+import React from 'react';
 
-const Footer = (props) => {
-  const {platform,icon,link} =props.linkData;
+const Footer = ({ linkData }) => {
   return (
     <div className='flex items-center justify-center mx-auto'>
         <article className='font-mono font-[600] text-center'>
             <div className='uppercase'>Lets Connect</div>
             <div>
-            <a href="mailto:ansonsd11@gmail.com">ansonsd11@gmail.com</a>
+            <a href="mailto:ansonsd11@gmail.com" rel="noopener noreferrer" alt="Email Me">ansonsd11@gmail.com</a>
             </div>
         
             <div className='flex gap-5 py-4'>
-              {socialConnections.map((plat) =>(
+              {linkData.map((plat) =>(
                 <div key={plat.platform}>
                   <a href={plat.link}  target="_blank" rel="noopener noreferrer">
-                  <img src={plat.icon} alt={plat.platform}/>
+                  <img src={plat.icon} alt={`Follow me on ${plat.platform}`}/>
                   </a>
                 </div>
               ))}
