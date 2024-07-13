343cdf7f3a92547e1299bcbe1f92c49baaf7e940
Ansonsd5 <anson11sd@gmail.com>
2024-07-13 20:18:12 +0530

    fix: years changes

--- src/utils/textConstant.js
+++ src/utils/textConstant.js
@@ -6,7 +6,7 @@ const ABOUTTEXT = {
 }
 
 const SELF_IDENTITY = {
-    ABOUTME : "With 1.5+ years as a front-end developer, I carry a passion for crafting exceptional digital experiences. My journey in the dynamic realm of web development is marked by hands-on expertise and a commitment to continual growth. I find immense joy in applying my skills to create compelling, user-friendly interfaces that resonate with users. Staying current with the latest advancements isn't just a priority; it's a thrilling pursuit. Beyond the code, I'm an avid learner, driven by the excitement of leveraging emerging technologies to innovate and shape meaningful digital journeys. I believe in the power of collaboration, and I'm eager to connect and embark together on the exciting and rewarding journey of web development!"
+    ABOUTME : "With 2+ years as a front-end developer, I carry a passion for crafting exceptional digital experiences. My journey in the dynamic realm of web development is marked by hands-on expertise and a commitment to continual growth. I find immense joy in applying my skills to create compelling, user-friendly interfaces that resonate with users. Staying current with the latest advancements isn't just a priority; it's a thrilling pursuit. Beyond the code, I'm an avid learner, driven by the excitement of leveraging emerging technologies to innovate and shape meaningful digital journeys. I believe in the power of collaboration, and I'm eager to connect and embark together on the exciting and rewarding journey of web development!"
 }
 
 const PROJECTS_OUTCOMES ={
